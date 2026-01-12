45e456861055e986eb855df9ed92ce9cca7096a5
Jinseok Park <j_seokpark@naver.com>
2026-01-12 19:51:34 +0900

    Feat(host): empty-card 컴포넌트 구현 (#47)
    * feat: empty-card 컴포넌트 구현
    
    * refactor: borderRadius 단위 변경
    
    * [Feat] 툴팁 컴포넌트 구현 (#41)
    
    * feat: 툴팁 컴포넌트 기본 구조 추가
    
    * feat: 툴팁 스타일 적용
    
    * feat: 포인터 아이콘 추가
    
    * refactor: rem 단위 적용
    
    * feat: z-index 추가
    
    * chore: 접근성 속성 정리
    
    * refactor: 구조 리팩토링
    
    * style: 코드 정리
    
    * refactor: 접근성 속성 추가
    
    * refactor: 포인터 아이콘 위치 조정
    
    * fix: --force로 옵션 수정 (#54)
    
    * Feat(ads): 칩 컴포넌트 구현 (#43)
    
    * feat: 칩 컴포넌트 타입 구조 설계
    
    * feat: vanilla-extract recipes 설치
    
    * feat: chip recipe 설정
    
    * feat: 칩 컴포넌트 구현
    
    * style: 일부 스타일 속성 변경
    
    * feat: ReactNode 타입 import 추가
    
    * feat: compoundVariants 제거
    
    * feat: borderRadius에 px 단위 사용
    
    * Fix(project): Vite 오류 해결 (#45)
    
    * fix: vite 오류 해결
    
    * refactor: ReferenceError 방지 설정 추가
    
    * refactor: 확장 className 삭제
    
    ---------
    
    Co-authored-by: 임지수 <dlawltn0708@naver.com>
    Co-authored-by: Naeun Lim <122524310+eunkr82@users.noreply.github.com>

---
+++ apps/host/src/shared/ui/card/empty-card/empty-card.css.ts
@@ -0,0 +1,15 @@
+import { style } from '@vanilla-extract/css';
+
+import { ampThemeVars } from '@amp/ads-ui/styles';
+
+export const emptyCard = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '1.2rem',
+  borderRadius: '16px',
+  border: `1px solid ${ampThemeVars.color.gray_100}`,
+  backgroundColor: ampThemeVars.color.gray_000,
+  color: ampThemeVars.color.gray_500,
+  ...ampThemeVars.font.title_sb_16,
+});
