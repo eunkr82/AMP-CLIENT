72cd8d0e5580b2ca30fdba228104987f1608e0d7
Naeun Lim <122524310+eunkr82@users.noreply.github.com>
2026-01-14 15:10:34 +0900

    Feat(audience) : AddToWatchButton 컴포넌트 구현 (#76)
    * feat: AddToWatchButton 컴포넌트 구현
    
    * remove:  불필요한 속성 제거

---
+++ packages/ads-ui/src/components/button/add-to-watch/add-to-watch.tsx
@@ -0,0 +1,24 @@
+import { AmpFlagIcon } from '../../../icons';
+
+import * as styles from './add-to-watch.css';
+
+interface AddToWatchButtonProps {
+  selected: boolean;
+  onChange: (nextSelected: boolean) => void;
+}
+
+const AddToWatchButton = ({ selected, onChange }: AddToWatchButtonProps) => {
+  return (
+    <button
+      type='button'
+      className={styles.addToWatch}
+      aria-pressed={selected}
+      onClick={() => onChange(!selected)}
+    >
+      <AmpFlagIcon />
+      관람 예정
+    </button>
+  );
+};
+
+export default AddToWatchButton;
--- packages/ads-ui/src/components/index.ts
+++ packages/ads-ui/src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as AddToWatchButton } from './button/add-to-watch/add-to-watch';
 export { default as CircleButton } from './button/circle-button/circle-button';
 export { default as CtaButton } from './button/cta-button/cta-button';
 export { default as FlagButton } from './button/flag-button/flag-button';
