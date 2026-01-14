27a7277e4d817e758ce280b5d83a27a43c794624
Jinseok Park <j_seokpark@naver.com>
2026-01-14 23:00:58 +0900

    Feat(ads): 공연 안내 카드 컴포넌트 구현 (#86)
    * feat: 공연 안내 카드 컴포넌트 구조 설계
    
    * feat: 기본 공연 안내 컴포넌트 구현
    
    * feat: 공지 안내 카드 컴포넌트 구조 변경 및 재구현
    
    * refactor: 컴파운드 패턴으로 구조 변경
    
    * feat: 말줄임표 구현
    
    * feat: 날짜 관련 로직 추가
    
    * refactor: api 명세 변경으로 인한 날짜 로직 단순화
    
    * feat: 마진 변경사항 반영
    
    * refactor: 이미지 관련 속성 추가
    
    * refactor: 컴파운드 패턴 구조 변경
    
    * refactor: 불필요 태그 제거

---
+++ packages/ads-ui/src/components/card/card-festival/card-festival.css.ts
@@ -0,0 +1,62 @@
+import { style } from '@vanilla-extract/css';
+
+import { ampThemeVars } from '../../../styles';
+
+export const card = style({
+  display: 'flex',
+  position: 'relative',
+  gap: '1.6rem',
+  backgroundColor: ampThemeVars.color.gray_000,
+  borderRadius: '16px',
+  border: `1px solid ${ampThemeVars.color.gray_200}`,
+  padding: '0.9rem 1rem',
+});
+
+export const image = style({
+  width: '6.3rem',
+  height: '8.4rem',
+  borderRadius: '8px',
+  flexShrink: 0,
+  objectFit: 'cover',
+});
+
+export const contentContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.8rem',
+});
+
+export const title = style({
+  width: '18.1rem',
+  color: ampThemeVars.color.gray_900,
+  ...ampThemeVars.font.title_sb_18,
+  display: '-webkit-box',
+  WebkitBoxOrient: 'vertical',
+  WebkitLineClamp: 1,
+  overflow: 'hidden',
+});
+
+export const duration = style({
+  color: ampThemeVars.color.gray_500,
+  ...ampThemeVars.font.body_m_13,
+});
+
+export const buttonSlot = style({
+  position: 'absolute',
+  right: '1rem',
+  top: '50%',
+  transform: 'translateY(-50%)',
+});
+
+export const iconSlot = style({
+  position: 'absolute',
+  top: '1.25rem',
+  right: '1rem',
+  width: '2.2rem',
+  height: '2.2rem',
+});
+
+export const chip = style({
+  display: 'flex',
+  gap: '0.5rem',
+});
