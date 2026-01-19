3e9958919cc499aae8db3b7b07258b542ba43352
Jinseok Park <j_seokpark@naver.com>
2026-01-19 22:52:26 +0900

    Feat(project): PWA 설정 (#142)

--- apps/audience/vite.config.ts
+++ apps/audience/vite.config.ts
@@ -2,6 +2,7 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 import { baseViteConfig } from '@amp/vite-config';
 
@@ -14,5 +15,43 @@ export default defineConfig(
     server: {
       port: 5173,
     },
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true,
+        },
+
+        includeAssets: [
+          'favicon.svg',
+          'amp-pwa-logo-192.png',
+          'amp-pwa-logo-512.png',
+        ],
+        manifest: {
+          name: 'AMP',
+          short_name: 'AMP',
+          description: '작은 공지도 크게 울리게 공연 공지의 공식, AMP',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'amp-pwa-logo-192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'amp-pwa-logo-512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'amp-pwa-logo-512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+      }),
+    ],
   }),
 );
